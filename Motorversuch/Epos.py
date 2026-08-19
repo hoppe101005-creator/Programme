5bf95e166c7f47a9f034deba15835d826cc29e3c
hoppe101005-creator <hoppe101005@gmail.com>
2026-08-19 09:50:43 +0200

    test_epos.py erstellt

--- Motorversuch/Epos.py
+++ Motorversuch/Epos.py
@@ -3,3 +3,10 @@ from ctypes import *
 epos = windll.LoadLibrary(r"C:\Program Files (x86)\maxon motor ag\EPOS IDX\EPOS4\04 Programming\Windows DLL\LabVIEW\maxon EPOS\Resources\EposCmd64.dll")
 
 print ("Epos Command Library geladen")
+
+error_code = c_uint()
+
+handle = epos.VCS_OpenDevice(b"EPOS4", b"MAXON SERIAL V2", b"USB", b"USB0", byref(error_code))
+
+print (handle)
+print ("Error code: ", error_code.value)
