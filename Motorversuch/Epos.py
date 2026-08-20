e88b8c064774b72f1b03d6f3f2f4897a94ccc15a
hoppe101005-creator <hoppe101005@gmail.com>
2026-08-20 10:43:40 +0200

    Visu.py

--- Motorversuch/Epos.py
+++ Motorversuch/Epos.py
@@ -1,5 +1,9 @@
 from ctypes import *
+import keyboard
 
+end_position_lose = 0
+end_position_gespannt = 0
+position_werkstück_wechsel = 0
 dll_path = r"C:\Program Files (x86)\maxon motor ag\EPOS IDX\EPOS4\04 Programming\Windows DLL\LabVIEW\maxon EPOS\Resources\EposCmd64.dll"
 
 epos = WinDLL(dll_path)
@@ -35,13 +39,53 @@ handle = epos.VCS_OpenDevice(
     byref(error_code)
     )
 
-buffer = create_string_buffer(256)
+node_id = 1
+def aktuelle_position_auslesen(handle, node_id):
+    #Funktionsdefinition
+    epos.VCS_GetPositionIs.argtypes = [
+        c_void_p,
+        c_ushort,
+        POINTER(c_int),
+        POINTER(c_uint)
+    ]
 
-epos.VCS_GetErrorInfo(error_code.value, buffer, 256)
+    epos.VCS_GetPositionIs.restype = c_int
 
-print ("Handle: ", handle)
-print ("Handle repr: ", repr(handle))
-print ("Handle type: ", type(handle))
-print ("Error code: ", error_code.value)
-print ( "Fehlercode: ", buffer.value.decode("latin1"))
-print (buffer.value.decode(errors="ignore"))
+    current_position = c_int()
+
+    result = epos.VCS_GetPositionIs(
+        handle,
+        node_id,
+        byref(current_position),
+        byref(error_code)
+    )
+
+    if result:
+        position = current_position.value
+        print(f"Aktuelle Position: {position}")
+        return position
+    else:
+        print(f"Fehler: {error_code.value}")
+    
+
+def gehe_zu_position(handle, node_id, position):
+    epos.VCS_MoveToPosition(
+        handle, node_id, position, 1, 1, byref(error_code)
+    )
+    
+def position_einlesen(dokument):
+    with open (dokument, "r") as f:
+        position = int(f.read())
+        print("Position:", position)
+        return position
+    
+def position_speichern(dokument, position):
+    with open(dokument, "w") as f:
+            f.write(str(position))
+            print("Position", position, "wurde in ", dokument, "gespeichert")
+            
+
+
+if 
+    end_position_lose = aktuelle_position_auslesen(handle, node_id)
+    position_speichern("end_position_lose.txt", end_position_lose)
---
+++ Motorversuch/funktionen.py
@@ -0,0 +1,212 @@
+import ctypes
+from ctypes import *
+from pathlib import Path
+
+dll_path = r"C:\Program Files (x86)\maxon motor ag\EPOS IDX\EPOS2\04 Programming\Windows DLL\LabVIEW\maxon EPOS\Resources\EposCmd64.dll"
+
+epos = ctypes.WinDLL(dll_path)
+
+def pruefe_dateipfad(dateipfad):
+    return Path(dateipfad).exists()
+
+def list_port_names():
+    buffer = create_string_buffer(256)
+    end = c_int()
+    error = c_uint()
+
+    result = epos.VCS_GetPortNameSelection(
+        b"EPOS4",
+        b"MAXON SERIAL V2",
+        b"USB",
+        True,
+        buffer,
+        256,
+        byref(end),
+        byref(error)
+)
+
+    while result:
+        print("Port:", buffer.value.decode())
+
+        result = epos.VCS_GetPortNameSelection(
+            b"EPOS4",
+            b"MAXON SERIAL V2",
+            b"USB",
+            False,
+            buffer,
+            256,
+            byref(end),
+            byref(error)
+    )
+
+def list_interfaces():
+    error = c_uint()
+    end = c_int()
+    buffer = create_string_buffer(256)
+
+    # Interface-Namen für USB anzeigen
+
+    result = epos.VCS_GetInterfaceNameSelection(
+        b"EPOS4",
+        b"MAXON SERIAL V2",
+        True,
+        buffer,
+        256,
+        byref(end),
+        byref(error)
+    )
+
+    while result:
+        print("Interface:", buffer.value.decode())
+
+        result = epos.VCS_GetInterfaceNameSelection(
+            b"EPOS4",
+            b"MAXON SERIAL V2",
+            False,
+            buffer,
+            256,
+            byref(end),
+            byref(error)
+        )
+        
+
+def list_protocols():
+    name = create_string_buffer(256)
+    end = c_int()
+    error = c_uint()
+
+    start = 1
+
+    while True:
+
+            result = epos.VCS_GetProtocolStackNameSelection(
+                b"EPOS4",
+                start,
+                name,
+                256,
+                byref(end),
+                byref(error)
+            )
+
+            if result == 0:
+                break
+
+            print("Protocol:", name.value.decode())
+
+            if end.value:
+                break
+
+            start = 0
+
+
+def list_devices():
+    name = create_string_buffer(256)
+    end = c_int()
+    error = c_uint()
+
+    epos.VCS_GetDeviceNameSelection.argtypes = [
+        c_ushort,
+        c_char_p,
+        c_ushort,
+        POINTER(c_int),
+        POINTER(c_uint)
+    ]
+
+    start = 1
+
+    while True:
+
+        result = epos.VCS_GetDeviceNameSelection(
+            start,
+            name,
+            256,
+            byref(end),
+            byref(error)
+        )
+
+        if result == 0:
+            break
+
+        print("Device:", name.value.decode())
+
+        if end.value:
+            break
+
+        start = 0
+
+
+def find_device():
+    error = c_uint(0)
+
+    device = create_string_buffer(64)
+    protocol = create_string_buffer(64)
+    interface = create_string_buffer(64)
+    port = create_string_buffer(64)
+
+    baudrate = c_uint()
+    timeout = c_uint()
+    nodeid = c_ushort()
+    handle = c_void_p()
+
+    result = epos.VCS_FindDeviceCommunicationSettings(
+        byref(handle),
+        device,
+        protocol,
+        interface,
+        port,
+        64,
+        byref(baudrate),
+        byref(timeout),
+        byref(nodeid),
+        1,
+        byref(error)
+    )
+
+    buffer = create_string_buffer(256)
+
+    epos.VCS_GetErrorInfo(
+        error.value,
+        buffer,
+        256
+    )
+
+    print(buffer.value.decode())
+    print("Result:", result)
+    print("Handle:", handle.value)
+    print("Device:", device.value.decode())
+    print("Protocol:", protocol.value.decode())
+    print("Interface:", interface.value.decode())
+    print("Port:", port.value.decode())
+    print("Baudrate:", baudrate.value)
+    print("NodeID:", nodeid.value)
+    print("Error:", error.value)
+    return error
+
+def fehlercode_ausgeben(error_code):
+    buffer = create_string_buffer(256)
+    
+    epos.VCS_GetErrorInfo(
+        error_code,
+        buffer,
+        256
+    )
+
+    print("Fehlermeldung: ", buffer.value.decode("latin1"))
+    
+ def rueckgabe():
+    
+    error_code = c_uint(0)
+    buffer = create_string_buffer(256)
+    epos.VCS_GetErrorInfo(error_code.value, buffer, 256)
+    print ("Handle: ", handle)
+    print ("Handle repr: ", repr(handle))
+    print ("Handle type: ", type(handle))
+    print ("Error code: ", error_code.value)
+    print ( "Fehlercode: ", buffer.value.decode("latin1"))
+    print (buffer.value.decode(errors="ignore"))
+    print("Der Pfad existiert: ", pruefe_dateipfad(dll_path))
+    fehlercode_ausgeben(find_device())
+    list_devices()
+    list_protocols()
+    list_interfaces()
+    list_port_names()
